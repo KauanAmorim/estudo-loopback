3c3107fa62223cef0ec168adbe364f987e9006e0
KauanAmorim <kauanamorim88@gmail.com>
2020-12-27 16:26:06 -0300

    adicionando o primeiro controller hello world

--- src/controllers/hello.controller.ts
+++ src/controllers/hello.controller.ts
@@ -2,7 +2,10 @@
 
 // import {inject} from '@loopback/core';
 
-
+import {get} from '@loopback/rest';
 export class HelloController {
-  constructor() {}
+  @get('/hello')
+  hello(): string {
+    return 'Hello world!';
+  }
 }
